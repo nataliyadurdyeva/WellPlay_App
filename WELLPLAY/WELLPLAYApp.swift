45d066809650fe57e70c40daa3a11fc3471f9819
nataliyadurdyeva <107269455+nataliyadurdyeva@users.noreply.github.com>
2022-09-15 00:13:04 -0700

    Initial Commit

---
+++ WELLPLAY/WELLPLAYApp.swift
@@ -0,0 +1,17 @@
+//
+//  WELLPLAYApp.swift
+//  WELLPLAY
+//
+//  Created by Nataliya Durdyeva on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WELLPLAYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
