3c085aedeaf871927a331621545a9ec4d4fd31ee
nataliyadurdyeva <107269455+nataliyadurdyeva@users.noreply.github.com>
2022-09-29 14:37:40 -0700

    Email Support works

--- WELLPLAY/MainViews/SearchView.swift
+++ WELLPLAY/MainViews/SearchView.swift
@@ -8,6 +8,7 @@ import Foundation
 import SwiftUI
 import Firebase
 import Kingfisher
+import UIKit
 
 struct SearchView: View {
     @EnvironmentObject private var viewModel: AppViewModel
@@ -15,18 +16,19 @@ struct SearchView: View {
     init() {
         
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.green]
-  
+        
     }
     
-    
+    private var email = SupportEmail(toAddress: "natalyadu@gmail.com", subject: "Support Email", messageHeader: "Please describe your issue below")
     @State private var userName = ""
     @State private var age = ""
     @State private var location = ""
     @State private var sports = ""
     @State var searchText = ""
     @State var shouldShowLogOutOptions = false
+    @Environment(\.openURL) var openURL
     let searchRectangleColor = Color(red: 0.68, green: 0.81, blue: 0.92)
-//    var user: User?
+    //    var user: User?
     
     var filteredUsers: [User] {
         if searchText.isEmpty {
@@ -39,24 +41,33 @@ struct SearchView: View {
     var body: some View {
         
         NavigationView {
-
+            
             VStack {
                 HStack (alignment: .bottom) {
-                Text("FIND SPORT BUDDIES NEAR YOU!")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    Text("FIND SPORT BUDDIES NEAR YOU!")
+                        .font(.headline)
+                        .foregroundColor(.white)
                     Button(role: .none) {shouldShowLogOutOptions = true
                     } label: {
-                                Image(systemName: "gear")
+                        Image(systemName: "gear")
                             .font(.system(size: 12, weight: .bold))
                             .foregroundColor(Color(.white))
-                            }
+                    }
                     .alert("What do you want to do?", isPresented: $shouldShowLogOutOptions) {
                         Button("Sign Out", action: {viewModel.signout()})
                         Button("Delete Account", role: .destructive, action: {viewModel.deleteUser()})
                         Button("Cancel", role: .cancel, action: {})
                     }
-            } .background(Color("DarkBlue"))
+                } .background(Color("DarkBlue"))
+                Button {
+                    email.send(openURL: openURL)
+                } label: {
+                    HStack {
+                        Text("Email Support")
+                        Image(systemName: "envelope.circle.fill")
+                            .font(.title2)
+                    }
+                }
                 ZStack {
                     Rectangle()
                         .foregroundColor(searchRectangleColor)
@@ -72,7 +83,7 @@ struct SearchView: View {
                     .padding()
                 
                 VStack {
-                   
+                    
                     List {
                         
                         ForEach(filteredUsers, id: \.self)
@@ -84,8 +95,8 @@ struct SearchView: View {
                         .listRowBackground(Color("DarkBlue"))
                     }
                     .listStyle(.plain)
-                        
-              
+                    
+                    
                 }
             }
             .onAppear() {
@@ -94,89 +105,120 @@ struct SearchView: View {
             }
             .navigationBarTitleDisplayMode(.inline)
             .background(Color("DarkBlue"))
-
-        }.navigationBarHidden(true)
-        
+            
+        }
+//        .navigationBarHidden(true)
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//
+//                }
+//
+//            }
     }
-}
-
-struct SearchCell: View {
     
-    let user: User
-    @EnvironmentObject private var viewModel: AppViewModel
-    
-    var body: some View {
+    struct SearchCell: View {
         
-        if user.id ?? "" !=  Auth.auth().currentUser?.uid  {
-            HStack  {
-                
-                VStack (alignment: .leading) {
+        let user: User
+        @EnvironmentObject private var viewModel: AppViewModel
+        
+        var body: some View {
+            
+            if user.id ?? "" !=  Auth.auth().currentUser?.uid  {
+                HStack  {
                     
-                    KFImage(URL(string: user.profilePictureUrl))
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .clipped()
-                        .scaledToFill()
-                        .clipShape(Rectangle())
-                        .cornerRadius(20)
-                        .shadow(radius: 5)
-                    
-                    Spacer()
-                    
-                    VStack (alignment: .leading, spacing: 4)  {
+                    VStack (alignment: .leading) {
                         
-                        Text("\(user.userName.capitalizingFirstLetter()), \(user.age)")
-                        
-                            .font(.system(size:15)).bold()
-                            .foregroundColor(.white)
-                        
-                        Text(user.location)
-                            .font(.system(size:15))
-                            .foregroundColor(.white)
-                        
-                    }
-                }.padding(25)
-                    .background(Color("DarkBlue"))
-                
-                VStack (alignment: .leading) {
-                    HStack {
-                        Text(user.sports.lowercased())
-                            .padding(.leading, 2)
-                            .font(.system(size:15))
-                            .foregroundColor(.white)
+                        KFImage(URL(string: user.profilePictureUrl))
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .clipped()
+                            .scaledToFill()
+                            .clipShape(Rectangle())
+                            .cornerRadius(20)
                             .shadow(radius: 5)
-                    }
-                    .foregroundColor(.white)
-                    .font(.caption2)
-                    .padding(4)
-                    .background(Color.blue.opacity(0.3)
-                        .cornerRadius(10))
-                    .padding(4)
-                    Spacer()
+                        
+                        Spacer()
+                        
+                        VStack (alignment: .leading, spacing: 4)  {
+                            
+                            Text("\(user.userName.capitalizingFirstLetter()), \(user.age)")
+                            
+                                .font(.system(size:15)).bold()
+                                .foregroundColor(.white)
+                            
+                            Text(user.location)
+                                .font(.system(size:15))
+                                .foregroundColor(.white)
+                            
+                        }
+                    }.padding(25)
+                        .background(Color("DarkBlue"))
                     
-                    Text(user.bio)
-                    
-                        .font(.system(size:15)).fontWeight(Font.Weight.medium)
-                        .lineSpacing(5)
+                    VStack (alignment: .leading) {
+                        HStack {
+                            Text(user.sports.lowercased())
+                                .padding(.leading, 2)
+                                .font(.system(size:15))
+                                .foregroundColor(.white)
+                                .shadow(radius: 5)
+                        }
                         .foregroundColor(.white)
-                        .multilineTextAlignment(.leading)
-                    
-                    Spacer()
-                    
-                    
-                    NavigationLink(destination: ChatLogView(reciever: user)){
-                        Text("Start Chat") .bold()
-                            .frame(maxWidth: 400, maxHeight: 40)
-                            .background(Color("FancyGreen"))
+                        .font(.caption2)
+                        .padding(4)
+                        .background(Color.blue.opacity(0.3)
+                            .cornerRadius(10))
+                        .padding(4)
+                        Spacer()
+                        
+                        Text(user.bio)
+                        
+                            .font(.system(size:15)).fontWeight(Font.Weight.medium)
+                            .lineSpacing(5)
                             .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .shadow(radius: 5)
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                        
+                        
+                        NavigationLink(destination: ChatLogView(reciever: user)){
+                            Text("Start Chat") .bold()
+                                .frame(maxWidth: 400, maxHeight: 40)
+                                .background(Color("FancyGreen"))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .shadow(radius: 5)
+                        }
+                        
                     }
-  
+                    
                 }
                 
             }
-
+        }
+    }
+    
+    struct SupportEmail {
+        let toAddress: String
+        let subject: String
+        let messageHeader: String
+        var body: String {"""
+If you are reporting user please tell us their username and the reason you are reporting them. Thank you!
+\(messageHeader)
+------------------------------------------------------------
+"""}
+        
+        
+        func send(openURL: OpenURLAction) {
+            let urlString = "mailto:\(toAddress)?subject=\(subject.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "")&body=\(body.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? "" )"
+            guard let url = URL(string: urlString) else {return}
+            openURL(url) { accepted in
+                if !accepted {
+                    print("""
+                    This device does not support email
+                    \(body)
+                    """)
+                }
+            }
         }
     }
 }
